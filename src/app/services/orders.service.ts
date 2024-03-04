af32aae771a387e66c2499b562a3676441eb93e3
Ahmed Tarek Hagar <ahmed.tarek.ronaldo@gmail.com>
2024-03-04 19:08:41 +0200

    minor fixes

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -10,7 +10,7 @@ export class OrdersService {
   constructor(private _HttpClient: HttpClient) { }
 
   baseURL: string = `https://ecommerce.routemisr.com`
-  redirectURL: string = `url=http://localhost:4200`
+  redirectURL: string = `url=https://ahmedtarekhagar.github.io/e-commerce-angular`
 
   creditCardCheckOut(cartId: string, addressFormValue: any): Observable<any> {
     return this._HttpClient.post(`${this.baseURL}/api/v1/orders/checkout-session/${cartId}?${this.redirectURL}`, { shippingAddress: addressFormValue });
